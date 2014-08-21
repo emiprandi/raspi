7fed20a5cecc860b996537c853644f078499d493
Emiliano Prandi <emiprandi@gmail.com>
2014-08-21 15:10:17 -0300

    random play

--- app.js
+++ app.js
@@ -20,9 +20,13 @@ raspi.init = function () {
     raspi.log('Servidor', 'OK');
     spotify.login(k.spotify_user, k.spotify_pass, false, false);
 }
-raspi.player.playNextSong = function () {
+raspi.playerPlayNextSong = function () {
     if (app.player.queue.length > 0) {
-        var track = app.player.queue.shift();
+        //var track = app.player.queue.shift();
+        var trackNro = raspi.randomInt(0, app.player.queue.length);
+            track = app.player.queue[trackNro];
+
+        delete app.player.queue[trackNro];
         
         spotify.player.play(track);
         raspi.log('Ahora suena: ' + track.name + ' de ' + track.artists[0].name, 'OK');
@@ -31,14 +35,14 @@ raspi.player.playNextSong = function () {
         raspi.log('No hay más canciones', 'OK');
     }
 }
-raspi.player.stop = function () {
+raspi.playerStop = function () {
     spotify.player.stop();
 }
 raspi.resetQueue = function () {
     app.player.queue = {};
 }
 raspi.randomInt = function (min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
+    return Math.floor(Math.random() * ((max+1) - min)) + min;
 }
 
 
@@ -49,7 +53,7 @@ spotify.on({
     ready: function () {
         app.playlists = spotify.playlistContainer.getPlaylists();
         for (var pl in app.playlists) {
-            app.playlists[pl].image = '/default_image_' + raspi.randomInt(1, 6) + '.png';
+            app.playlists[pl].image = '/default_image_' + raspi.randomInt(1, 5) + '.png';
         }
         raspi.log('Playlists', 'OK');
         raspi.log('Todo listo!', 'OK');
@@ -57,7 +61,7 @@ spotify.on({
 });
 spotify.player.on({
     endOfTrack: function () {
-        raspi.player.playNextSong();
+        raspi.playerPlayNextSong();
     }
 });
 
@@ -73,12 +77,12 @@ serv.get('/play/:pl', function(req, res) {
     var pl = spotify.createFromLink(req.params.pl);
     app.player.queue = pl.getTracks();
 
-    raspi.player.playNextSong();
+    raspi.playerPlayNextSong();
     res.status(200).end();
 });
 
 serv.get('/stop', function(req, res) {
-    raspi.player.stop();
+    raspi.playerStop();
     res.status(200).end();
 });
 
